575c53a7cff176d17e1cfad22c57595e15db31d0
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-06 15:22:42 -0500

    Added and Named Elements

--- app.js
+++ app.js
@@ -9,3 +9,28 @@
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
+const questionSection = document.getElementById('question-section');
+const answerSection = document.getElementById('answer-section');
+const button = document.getElementById('button');
+
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Outlook hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
